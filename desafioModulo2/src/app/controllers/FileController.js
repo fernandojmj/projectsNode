0999c6c8885de853feb03144c344a9c2d44cb483
Fernando <fernandojmj@gmail.com>
2020-04-23 00:22:46 -0300

    atualizações

--- desafioModulo2/src/app/controllers/FileController.js
+++ desafioModulo2/src/app/controllers/FileController.js
@@ -1,12 +1,12 @@
-const path = require('path')
+const path = require("path");
 
 class FileController {
-  show (req, res) {
-    const { file } = req.params
-    const filePath = path.resolve('tmp', 'uploads', file)
-    console.log(filePath)
-    return res.sendFile(filePath)
+  show(req, res) {
+    const { file } = req.params;
+    const filePath = path.basename("tmp", "uploads", file);
+    console.log(filePath);
+    return res.sendFile(filePath);
   }
 }
 
-module.exports = new FileController()
+module.exports = new FileController();
